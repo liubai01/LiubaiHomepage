7700975e539809e674a62044021580bf56d66fa3
Yintao, Xu <0@liubai01.cn>
2023-01-28 02:06:36 -0500

    [Fix] Upload nextcloud config

---
+++ server_data/nextcloud/config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'overwritewebroot' => '/data',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocoqincad44a',
+);
